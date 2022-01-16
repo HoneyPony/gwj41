f4a9895ec99b02a8cb82ac61e0e39a73de1ed787
unknown <>
2022-01-15 23:27:35 -0500

    Make the weeds take 3 hits

--- env/DashWeedDense.gd
+++ env/DashWeedDense.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-var weeds_left = 3
+var weeds_left = 4
 
 var cooldown = 0.0
 var col_timer = -1
@@ -8,13 +8,17 @@ var del_timer = -1
 
 func was_hit():
 	#print("was hit")
+	if weeds_left == 4:
+		#$Weed3.queue_free()
+		$Particles2.emitting = true
+		del_timer = 0.2
 	if weeds_left == 3:
 		#$Weed3.queue_free()
-		$Particles0.emitting = true
+		$Particles1.emitting = true
 		del_timer = 0.2
 	if weeds_left == 2:
 		#$Weed2.queue_free()
-		$Particles1.emitting = true
+		$Particles0.emitting = true
 		#$Collision.queue_free()
 		col_timer = 0.3
 		del_timer = 0.2
@@ -38,7 +42,9 @@ func _physics_process(delta):
 	if del_timer > 0.0:
 		del_timer -= delta
 		if del_timer <= 0.0:
-			if weeds_left == 2:
+			if weeds_left == 3:
+				$Weed4.queue_free()
+			elif weeds_left == 2:
 				$Weed3.queue_free()
 			else:
 				$Weed2.queue_free()
