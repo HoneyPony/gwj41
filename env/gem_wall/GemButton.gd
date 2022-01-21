30891b70ec6ca7592d0a7bec23d50935f0a5fdb5
unknown <>
2022-01-21 16:10:51 -0500

    Add wall based sprocket

--- env/gem_wall/GemButton.gd
+++ env/gem_wall/GemButton.gd
@@ -6,7 +6,12 @@ export(NodePath) onready var target_door = GS.nodefp(self, target_door)
 
 var is_pressed = false
 
+export var stay_pressed = true
+
 func make_pressed():
+	if is_pressed:
+		return
+	
 	is_pressed = true
 	$AnimationPlayer.play("TurnOn")
 	
@@ -15,11 +20,20 @@ func make_pressed():
 
 func _physics_process(delta):
 	if is_pressed:
-		return
-		
+		if stay_pressed:
+			return
+			
 	var items = $Itemcheck.get_overlapping_bodies()
 	for item in items:
 		if item.is_in_group("GemGreen"):
 			if item.velocity.length() < 1:
 				make_pressed()
 				return
+				
+	if items.empty():
+		if is_pressed:
+			is_pressed = false
+			$AnimationPlayer.play("TurnOff")
+			
+			if target_door is Node:
+				target_door.get_node("AnimationPlayer").play("Close")
